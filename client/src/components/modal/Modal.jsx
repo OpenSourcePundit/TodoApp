3bd8ac9dea021004022d492fc8e99048851fc72d
Shashank Kumar <shashank00098@gmail.com>
2024-04-28 14:53:05 +0530

    feat: modal structure implemented

--- client/src/components/modal/Modal.jsx
+++ client/src/components/modal/Modal.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-const Modal = () => {
+function TodoModal({show, handleClose, title, description }) {
+ 
   return (
-    <div>Modal</div>
-  )
+    <>      
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading:{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!{description}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>  
+    </>
+  );
 }
 
-export default Modal
+export default TodoModal;
--- client/src/reducer/Reducer.js
+++ client/src/reducer/Reducer.js
@@ -1,40 +1,24 @@
 export const dataReducer = (state, { type, payload }) => {
 
     switch (type) {
-        case "UPDATE_WEATHER_DATA":
-            return {
-                ...state,
-                status:"success",
-                lat: payload.lat !== null ? payload.lat : state.lat,
-                lon: payload.lon !== null ? payload.lon : state.lon,
-                timezone: payload.timezone !== "" ? payload.timezone : state.timezone,
-                timezone_offset: payload.timezone_offset !== null ? payload.timezone_offset : state.timezone_offset,
-                current: {
-                    ...state.current,
-                    dt: payload.current.dt !== null ? payload.current.dt : state.current.dt,
-                    sunrise: payload.current.sunrise !== null ? payload.current.sunrise : state.current.sunrise,
-                    sunset: payload.current.sunset !== null ? payload.current.sunset : state.current.sunset,
-                    temp: payload.current.temp !== null ? payload.current.temp : state.current.temp,
-                    feels_like: payload.current.feels_like !== null ? payload.current.feels_like : state.current.feels_like,
-                    pressure: payload.current.pressure !== null ? payload.current.pressure : state.current.pressure,
-                    humidity: payload.current.humidity !== null ? payload.current.humidity : state.current.humidity,
-                    dew_point: payload.current.dew_point !== null ? payload.current.dew_point : state.current.dew_point,
-                    uvi: payload.current.uvi !== null ? payload.current.uvi : state.current.uvi,
-                    clouds: payload.current.clouds !== null ? payload.current.clouds : state.current.clouds,
-                    visibility: payload.current.visibility !== null ? payload.current.visibility : state.current.visibility,
-                    wind_speed: payload.current.wind_speed !== null ? payload.current.wind_speed : state.current.wind_speed,
-                    wind_deg: payload.current.wind_deg !== null ? payload.current.wind_deg : state.current.wind_deg,
-                    wind_gust: payload.current.wind_gust !== null ? payload.current.wind_gust : state.current.wind_gust,
-                    weather: payload.current.weather.length > 0 ? payload.current.weather : state.current.weather,
-                },
-                daily: payload.daily.length > 0 ? payload.daily : state.daily,
-            };
-        case "TOGGLE_UNIT":
-            return {
-                ...state,
-                unit: state.unit === "metric" ? "imperial" : "metric"
-            };
-
+        case "ADD_TASK":
+            let updatedArray;
+            const { id, title, description, status } = action.payload;
+            const newObj = { id, title, description };
+            switch (status) {
+                case 'To Do':
+                    updatedArray = [...state.todo, newObj];
+                    return { ...state, todo: updatedArray };
+                case 'Doing':
+                    updatedArray = [...state.doing, newObj];
+                    return { ...state, doing: updatedArray };
+                case 'Done':
+                    updatedArray = [...state.done, newObj];
+                    return { ...state, done: updatedArray };
+                    default:
+            return state;
+            }
+        
 
 
         case "SET_LOCATION":
